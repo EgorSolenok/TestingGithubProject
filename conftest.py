9333c6dad0aa680a5361c8bd5493873c5c63ae70
solenok_egor <solenok.egor@gmail.com>
2021-12-03 20:27:30 +0300

    Working at screenshots Allure of faliures

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import logging
 
 import pytest
+import allure
+import time
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -20,7 +23,7 @@ def pytest_addoption(parser):
 
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="session")
 def browser(request):
     '''
     Method automatically initializes driver for Chrome browser.
@@ -32,7 +35,34 @@ def browser(request):
         'prefs',{'intl.accept_languages': user_language})
 
     browser = webdriver.Chrome(ChromeDriverManager(log_level=logging.ERROR).install(), options=options)
+
     yield browser
+
+    # if request.node.rep_call.failed:
+    #     # Make the screen-shot if test failed:
+    #     try:
+    #         browser.execute_script("document.body.bgColor = 'white';")
+    #
+    #         allure.attach(browser.get_screenshot_as_png(),
+    #                       name=request.function.__name__,
+    #                       attachment_type=allure.attachment_type.PNG)
+    #     except:
+    #         browser.execute_script("document.body.bgColor = 'white';")
+    #
+    #         allure.attach(browser.get_screenshot_as_png(),
+    #                       name=request.function.__name__,
+    #                       attachment_type=allure.attachment_type.PNG)
     browser.quit()
+
+# @pytest.hookimpl(hookwrapper=True, tryfirst=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     rep = outcome.get_result()
+#     setattr(item, "rep_" + rep.when, rep)
+#     return rep
+
+
+
+
 
 
